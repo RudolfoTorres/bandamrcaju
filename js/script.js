dab7d32c1f44248f42bd98a044de8331c23f4f84
Rudolfo Torres <rudolfotorres@gmail.com>
2022-06-25 13:14:29 -0300

    insercao do player no mobile

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 let navbar = document.querySelector('.navbar');
-
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
+    player.classList.remove('active');
+}
+
+let player = document.querySelector('iframe');
+document.querySelector('#player-btn').onclick = () => {
+    player.classList.toggle('active');
+    navbar.classList.remove('active');
 }
 
 window.onscroll = () => {
     navbar.classList.remove('active');
+    player.classList.remove('active');
 }
