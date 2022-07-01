f4e5fc941c9191084c20dab9ce419889f37f11db
Rudolfo Torres <rudolfotorres@gmail.com>
2022-07-01 12:42:06 -0300

    insercao da section schedule

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
+//ativar e desativar menu
 let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     player.classList.remove('active');
 }
 
-let player = document.querySelector('iframe');
+//ativar e desativar player
+let player = document.querySelector('#iframePlayer');
 document.querySelector('#player-btn').onclick = () => {
     player.classList.toggle('active');
     navbar.classList.remove('active');
 }
 
+//ativar e desativar menu e player no scroll
 window.onscroll = () => {
     navbar.classList.remove('active');
     player.classList.remove('active');
 }
 
+//conf da lib lightbox
 lightbox.option({
     'albumLabel': "Imagem %1 de %2",
     'alwaysShowNavOnTouchDevices': true,
     'wrapAround': true,
     'disableScrolling': true
 });
+
+//alterar url do maps
+$('.btnPlace').on('click', function(){
+    var urlPlace = this.value;
+    document.getElementById('iframeMap').src = urlPlace;
+});
+
